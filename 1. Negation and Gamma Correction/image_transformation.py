598b51a87078a48d702b48d9812e4be933d77103
Novian Deny <anggerdeni@gmail.com>
2019-09-04 23:00:19 +0700

    update script

--- 1. Negation and Gamma Correction/image_transformation.py
+++ 1. Negation and Gamma Correction/image_transformation.py
@@ -1,17 +1,50 @@
+import sys
 from PIL import Image
 
-def toGrayScale(img):
-  w,h = img.size
-  # Create new Image with mode L : gray scale
-  grayScaleImage = Image.new('L', (w, h))
-  for i in range(w):
-    for j in range(h):
-      pixelValue = int(sum(img.getpixel((i,j)))/3)
-      grayScaleImage.putpixel((i, j), (pixelValue))
-  
-  return grayScaleImage
+def main():
+  # Windows style
+  if len(sys.argv) < 2 or sys.argv[1] not in ['negation', 'gamma']:
+    print 'Usage: %s %s' % (sys.argv[0], '<action>')
+    print 'Available action:\n  %s\n  %s' % ('negation [optional:<image>]', 'gamma <gamma value>, [optional:<image>]')
+    print '\n\n\n'
+    sys.exit()
+  if sys.argv[1] == 'gamma' and len(sys.argv) < 3:
+    print 'Usage: %s %s %s %s' % (sys.argv[0], 'gamma', '<gamma value>', '<image target (optional)>')
+    sys.exit()
 
-def negateImg(img):
+  if sys.argv[1] == 'negation' and len(sys.argv) < 2:
+    print 'Usage: %s %s %s' % (sys.argv[0], 'negation', '<image target (optional)>')
+    sys.exit()
+
+
+  if(sys.argv[1] == 'negation'):
+    if(len(sys.argv) == 2):
+      negation()
+    else:
+      negation(sys.argv[1])
+  else:
+    if(len(sys.argv) == 3):
+      gamma(float(sys.argv[2]))
+    else:
+      gamma(float(sys.argv[2]), sys.argv[3])
+
+
+def negation(imgFileName = 'pic.jpg'):
+  img = negateImg(imgFileName)
+  img.show()
+
+def gamma(gammaValue = 1, imgFileName = 'pic.jpg'):
+  img = gammaCorrection(gammaValue, imgFileName)
+  img.show()
+
+def negateImg(imgFileName):
+  try:
+    img = Image.open(imgFileName)
+  except:
+    print "Error occured, unable to open file"
+    sys.exit()
+
+  img = toGrayScale(img)
   mode = img.mode
   w,h = img.size
   for i in range(w):
@@ -25,42 +58,40 @@ def negateImg(img):
         b = 255 - original[2]
         newPixels = (r,g,b)
       img.putpixel((i,j), newPixels)
+  return img
 
+def toGrayScale(img):
+  w,h = img.size
+  # Create new Image with mode L : gray scale
+  grayScaleImage = Image.new('L', (w, h))
+  for i in range(w):
+    for j in range(h):
+      pixelValue = int(sum(img.getpixel((i,j)))/3)
+      grayScaleImage.putpixel((i, j), (pixelValue))
+  
+  return grayScaleImage
 
-def gammaCorrection(img, gamma = 1):
+def gammaCorrection(gammaValue, imgFileName):
+  try:
+    img = Image.open(imgFileName)
+  except:
+    print "Error occured, unable to open file"
+    sys.exit()
+  img = toGrayScale(img)
   w,h = img.size
   mode = img.mode
   for i in range(w):
     for j in range(h):
       if mode == 'L':
-        newPixel = int(((img.getpixel((i, j))/255.0) ** gamma) * 255) 
+        newPixel = int(((img.getpixel((i, j))/255.0) ** gammaValue) * 255) 
         img.putpixel((i,j), (newPixel))
       else:
-        r = int(((img.getpixel((i, j))[0]/255.0) ** gamma) * 255) 
-        g = int(((img.getpixel((i, j))[1]/255.0) ** gamma) * 255) 
-        b = int(((img.getpixel((i, j))[2]/255.0) ** gamma) * 255) 
+        r = int(((img.getpixel((i, j))[0]/255.0) ** gammaValue) * 255) 
+        g = int(((img.getpixel((i, j))[1]/255.0) ** gammaValue) * 255) 
+        b = int(((img.getpixel((i, j))[2]/255.0) ** gammaValue) * 255) 
         newPixel = (r, g, b)
         img.putpixel((i,j), newPixel)
-
-
-def main():
-  # negate RGB image: save to negated.jpg
-  img = Image.open('pic.jpg')
-  negateImg(img)
-  img.save('negated.jpg')
-
-  # turn into grayscale then negate it: save to negated_grayscale.jpg
-  grayScaleImage = toGrayScale(img)
-  negateImg(grayScaleImage)
-  grayScaleImage.save('negated_grayscale.jpg')
-
-  # gamma correction
-  gamma = float(input('Masukkan nilai gamma: '))
-
-  img = Image.open('pic.jpg')
-  grayScaleImage = toGrayScale(img)
-  gammaCorrection(grayScaleImage, gamma)
-  grayScaleImage.save('gammaed.jpg')
+  return img
 
 if __name__ == '__main__':
   main()
